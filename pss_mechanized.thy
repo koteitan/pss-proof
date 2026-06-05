335642d1f02edc068e909ca02805024b8b9d48f0
koteitan <ukaibutton@mac.com>
2026-06-05 10:15:02 +0900

    Bank RedCondA_Pred and RedCondA_oper_nontiling (ST_PS=>RedCondA non-tiling cases)
    - Add RedCondA_Pred: RedCondA M => RedCondA (Pred M) (Pred is an M-prefix slice)
    - Add RedCondA_oper_nontiling: the three non-tiling oper branches of ST_PS.induct
      preserve RedCondA (M[n] in {M, Pred M} there)
    - The remaining ST_PS=>RedCondA obligation is the tiling oper branch, i.e. the
      §6.7 standard=>reduced structural content (p_6_7_standard_reduced)

--- pss_mechanized.thy
+++ pss_mechanized.thy
@@ -49334,6 +49334,56 @@ proof -
 qed
 
 
+subsection \<open>Front B (tag pss-stps-condA): \<open>ST_PS \<Longrightarrow> RedCondA\<close> bricks\<close>
+
+text \<open>\<open>Pred\<close> preserves \<open>RedCondA\<close>.  \<open>Pred M = butlast M = seg M 0 (Lng M - 2)\<close>
+  is an \<open>M\<close>-prefix; \<open>RedCondA\<close> is inherited by every slice with last index
+  \<open>< Lng M\<close> (@{thm [source] fa_RedCondA_seg}).  For \<open>Lng M \<le> 1\<close>, \<open>Pred M = M\<close>.\<close>
+
+lemma RedCondA_Pred:
+  assumes MT: "M \<in> T_PS" and condA: "RedCondA M"
+  shows "RedCondA (Pred M)"
+proof (cases "Lng M \<le> 1")
+  case True
+  thus ?thesis using condA by (simp add: Pred_def)
+next
+  case False
+  hence L: "Lng M > 1" by simp
+  let ?b = "Lng M - 2"
+  have sb: "Suc ?b \<le> Lng M" using L by simp
+  have ar: "Suc ?b = Lng M - 1" using L by simp
+  have pred_seg: "Pred M = seg M 0 ?b"
+    using seg_0_eq_take[OF sb] L ar by (simp add: Pred_def butlast_conv_take)
+  have segT: "seg M 0 ?b \<in> T_PS" using pred_seg Pred_preserves_T_PS[OF MT] by simp
+  have bL: "?b < Lng M" using L by simp
+  show ?thesis
+    using fa_RedCondA_seg[OF MT segT bL condA] pred_seg by simp
+qed
+
+text \<open>In the NON-TILING branches of \<open>oper\<close> (the three degenerate cases of §5.3:
+  \<open>Lng M = 1\<close>, last column \<open>= (0,0)\<close>, or no unique row-\<open>i\<^sub>1\<close> parent of the last
+  column), \<open>M[n] \<in> {M, Pred M}\<close>, so \<open>RedCondA\<close> is preserved by
+  @{thm [source] RedCondA_Pred}.  The ONLY remaining branch is the genuine
+  tiling expansion (\<open>take j\<^sub>0 M @ \<Oplus>\<^sub>k ...\<close>).\<close>
+
+lemma RedCondA_oper_nontiling:
+  assumes MT: "M \<in> T_PS" and condA: "RedCondA M" and n1: "1 \<le> n"
+    and nontile: "Lng M - 1 = 0
+                  \<or> (entry M 0 (Lng M - 1) = 0 \<and> entry M 1 (Lng M - 1) = 0)
+                  \<or> \<not> hasParent M (idx1 M (Lng M - 1)) (Lng M - 1)"
+  shows "RedCondA ((M::pairseq)[n])"
+proof -
+  have "(M::pairseq)[n] = M \<or> (M::pairseq)[n] = Pred M"
+    using nontile by (auto simp: oper_def Let_def)
+  thus ?thesis
+  proof
+    assume "(M::pairseq)[n] = M"
+    thus ?thesis using condA by simp
+  next
+    assume "(M::pairseq)[n] = Pred M"
+    thus ?thesis using RedCondA_Pred[OF MT condA] by simp
+  qed
+qed
 
 
 end
