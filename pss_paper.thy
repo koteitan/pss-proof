35a958136f1853c5df5d8d5693fd68779206f1c5
koteitan <ukaibutton@mac.com>
2026-06-21 13:03:56 +0900

    §7.4: transcribe p_7_4_Mark_Trans_repr into pss_paper (article 2490, statement-only)
    Official article placement of the Mark-Trans representation proposition, in the
    RT_PS form matching the other §7.4 paper statements (general T_PS form awaits the
    §6 P-Red-equivariance).  Mechanized (fully proven) as m_7_4_Mark_Trans_repr in
    pss_wip.  Build green.

--- pss_paper.thy
+++ pss_paper.thy
@@ -1346,6 +1346,21 @@ lemma p_7_4_Trans_Mark_Pred:
             \<and> scb_decomp (Trans M) (fst sb) (flatBT (Mark M m)) (snd sb)"
   sorry
 
+text \<open>命題（\<open>Mark\<close>の\<open>Trans\<close>による表示） (§7.4, article 2490): for any
+  \<open>(M,m) \<in> T\<^bsub>PS\<^esub>\<^sup>Marked\<close>, set \<open>j\<^sub>1 := Lng M - 1\<close>; if \<open>j\<^sub>1 - m > 0\<close> then
+  \<open>Mark(M, m) = Trans((M\<^sub>j)\<^bsub>j=m\<^esub>\<^bsup>j\<^sub>1\<^esup>)\<close>, i.e. the marked value at column \<open>m\<close> equals
+  the \<open>Trans\<close> of the backward slice from \<open>m\<close> to the last column.  The article
+  reduces to \<open>M \<in> RT\<^bsub>PS\<^esub> \<inter> PT\<^bsub>PS\<^esub>\<close> via the \<open>(IncrFirst,Red,P)\<close>-invariances; as with
+  the other §7.4 propositions we transcribe the \<open>M \<in> RT\<^bsub>PS\<^esub>\<close> form (the general
+  \<open>T\<^bsub>PS\<^esub>\<close> form awaits the §6 \<open>P\<close>-\<open>Red\<close>-equivariance).  \<open>(M\<^sub>j)\<^bsub>j=m\<^esub>\<^bsup>j\<^sub>1\<^esup> = seg M m (Lng M - 1)\<close>.
+  Mechanized as \<open>m_7_4_Mark_Trans_repr\<close> in \<open>pss_wip\<close> (fully proven, no \<open>sorry\<close>).\<close>
+
+lemma p_7_4_Mark_Trans_repr:
+  assumes "(M, m) \<in> Marked" "M \<in> RT_PS"
+    and "m < Lng M - 1"
+  shows "Mark M m = Trans (seg M m (Lng M - 1))"
+  sorry
+
 text \<open>系（\<open>Trans\<close>の\<open>Mark\<close>と切片による表示） (§7.4): for any
   \<open>(M,m) \<in> RT\<^bsub>PS\<^esub>\<^sup>Marked\<close> (modelled by \<open>(M,m) \<in> Marked \<and> M \<in> RT\<^bsub>PS\<^esub>\<close>), if
   \<open>0 < m < Lng M - 1\<close> then there exist unique \<open>(s\<^sub>0,b\<^sub>0)\<close> such that
